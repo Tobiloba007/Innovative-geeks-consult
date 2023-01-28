74d1186b373edbb6d0086d63b4ad9a2808142eb2
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-01-28 08:42:26 +0100

    saturday commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './header.css'
 import { motion } from 'framer-motion'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 
 const Header = () => {
   return (
+    <div className='nav-wrapper'>
     <div className='nav-con'>  
         <div className="header-left">
             <h1>GEEKS LOGO</h1>
@@ -24,6 +26,43 @@ const Header = () => {
               CONTACT US
             </motion.button>
         </div>
+    </div>
+             
+                 {/* BOOTSTRAP NAVBAR */}
+<div className='boot-nav'>
+                 <nav class="navbar  navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">GEEKS LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><RxHamburgerMenu /></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav nav-links-div">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <hr className='hr-line'/>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+          <hr className='hr-line'/>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+          <hr className='hr-line'/>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+        <hr className='hr-line'/>
+        </li>
+        <li class="nav-item">
+        <button className='nav-btn'>Contact Us</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
     </div>
   )
 }
