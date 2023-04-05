ce5dc2958c4fb4fdc802ef635274dd764f6a9d5e
Olaniyan michael <olaniyanmichael0@gmail.com>
2023-04-05 08:04:25 +0100

    final commit

--- src/components/AboutTeam.js/AboutTeam.jsx
+++ src/components/AboutTeam.js/AboutTeam.jsx
@@ -2,6 +2,9 @@ import { collection, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { db } from '../../firebase-config';
 import "./aboutTeam.css"
+import img1 from '../../assets/teamMember1.jpg'
+import img2 from '../../assets/teamMember2.jpg'
+import img3 from '../../assets/teamMember3.jpg'
 
 const AboutTeam = () => {
   const [content, setContent] = useState([]);
@@ -31,31 +34,31 @@ const AboutTeam = () => {
 
         <div className="member-div member-1">
           <img  className='member-img'
-          src="https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+          src={img1} alt="" />
           <p className='member-name'>{about.abName1}</p>
           <p className='member-position'>{about.abPosition1}</p>
         </div>
 
         <div className="member-div member-2">
           <img  className='member-img'
-          src="https://images.unsplash.com/photo-1667665970124-2273c6ef3489?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXQlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          src={img2} alt="" />
           <p className='member-name'>{about.abName2}</p>
           <p className='member-position'>{about.abPosition2}</p>
         </div>
 
         <div className="member-div member-3">
           <img  className='member-img'
-          src="https://images.unsplash.com/photo-1667665970121-c3504c519cda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXQlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+          src={img3} alt="" />
           <p className='member-name'>{about.abName3}</p>
           <p className='member-position'>{about.abPosition3}</p>
         </div>
 
-        <div className="member-div member-4">
+        {/* <div className="member-div member-4">
           <img  className='member-img'
           src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
           <p className='member-name'>{about.abName4}</p>
           <p className='member-position'>{about.abPosition4}</p>
-        </div>
+        </div> */}
 
       </div>
     </div>
